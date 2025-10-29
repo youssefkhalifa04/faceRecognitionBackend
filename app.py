f732d0ff3a42deb171c6d6e5a0e137de08a94f73
Youssef <youssefkh.tv2@gmail.com>
2025-10-29 17:24:54 +0100

    .

--- app.py
+++ app.py
@@ -132,5 +132,5 @@ def verify_face():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
      
